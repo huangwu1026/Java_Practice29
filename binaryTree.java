cb16d045ec42012aa1e49c713220b2b8f952538d
huangwu1026 <2421294458@qq.com>
2020-01-01 23:00:25 +0800

    Java_Practice

---
+++ binaryTree.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean find(TreeNode root, TreeNode node){
+        if(root == null)
+            return false;
+        if(root == node)
+            return true;
+        if(find(root.left, node))
+            return true;
+        return find(root.right, node);
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null)
+            return root;
+        boolean pL = false, pR = false, qL = false, qR = false;
+        //某一个节点为当前树的根
+        if(root == p || root == q){
+            return root;
+        }
+        //同一边
+        if(find(root.left, p)){
+            pL = true;
+            pR = false;
+        }else{
+            pR = true;
+            pL = false;
+        }
+        if(find(root.left, q)){
+            qL = true;
+            qR = false;
+        }else{
+            qR = true;
+            qL = false;
+        }
+        //同一边
+        if(pL && qL){
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        else if(pR && qR){
+            return lowestCommonAncestor(root.right, p, q);
+        }else{
+            //两边
+            return root;
+        }
+
+    }
